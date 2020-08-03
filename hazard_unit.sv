3658a55ca51551dc3919838d7d062dedc82243d2
Ali Saei <alisaei90@gmail.com>
2020-08-03 17:22:32 +0430

    Update hazard_unit.sv

--- hazard_unit.sv
+++ hazard_unit.sv
@@ -1,21 +1,21 @@
 `timescale 1ns/1ns
 module hazardUnit (
-  output reg awrite, pc_write, control,
-  input [4:0] ID_EX_rt, rs, rt,
+  output reg IF_ID_write, pc_write, control,
+  input [4:0] ID_EX_rt, Rs, Rt,
   input ID_EX_memread, jmp, dst, memwrite, beq, bne
   );
   
   always@(*)begin
     if(ID_EX_memread & (ID_EX_rt != 5'b0) & (
-      ( (ID_EX_rt == rs)&(~jmp) ) |
-      ( (ID_EX_rt == rt)&(dst|beq|bne|memwrite) )     
+      ( (ID_EX_rt == Rs)&(~jmp) ) |
+      ( (ID_EX_rt == Rt)&(dst|beq|bne|memwrite) )     
     ))begin
-      awrite = 1'b0;
+      IF_ID_write = 1'b0;
       pc_write = 1'b0;
       control = 1'b0;
     end
     else begin
-      awrite = 1'b1;
+      IF_ID_write = 1'b1;
       pc_write = 1'b1;
       control = 1'b1;
     end
